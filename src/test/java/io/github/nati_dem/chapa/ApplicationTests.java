f71d45846cdce30efa0cd13acf616932e2d77254
Natnael D <natnael.demise@gmail.com>
2024-06-01 14:48:32 -0500

    initiall springboot app

---
+++ src/test/java/io/github/nati_dem/chapa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nati_dem.chapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
